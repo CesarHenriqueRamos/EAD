8e4623e1caaf4f0e5a81b9a82a8305ec9879cd91
Cesar Henrique Ramos <cesar252742@gmail.com>
2020-08-24 15:24:56 -0300

    Inicio do projeto

---
+++ classes/controller/homeController.php
@@ -0,0 +1,10 @@
+<?php
+	namespace controller;
+	use \views\mainView;
+
+	class homeController{
+		public function index(){	
+			mainView::render('home.php');
+		}
+	}
+?>
---
+++ pages/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto EAD</title>
+	<meta charset="viewport" content="width=device-width;initial-scale=1.0;maximum-scale=1.0">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>estilo/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo INCLUDE_PATH ?>estilo/style.css">
+</head>
+<body>
+<base base="<?php echo INCLUDE_PATH; ?>" />
+<header>
+	<div class="container">
+		
+		<div class="clear"></div>
+	</div><!--container-->
+</header>
